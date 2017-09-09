79018ed85b6d3b2e5bd6dc7ea91c2b41aeeacab9
Unknown <timpike22@gmail.com>
2017-09-09 13:55:29 -0700

    Logic Addition and HTML Mod
    Logic for creation of cards and fixed footer

---
+++ assets/javascript/logic.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+var userSearch;
+var j;
+var cardBuild = '<div class="col-1"></div><div class="col-2"><div class="card"><div class="card-body"><h4 class="card-title">Event</h4><p class="card-text"><ul><li>Description<li>Date<li>Time<li>Location</ul></p><a href="#"><img alt="Weather"class="img-thumbnail"height="50px"src="assets/images/weather.png"width="50px"></a></div></div></div><div class="col-2"><div class="card"><div class="card-body"><h4 class="card-title">Event</h4><p class="card-text"><ul><li>Description<li>Date<li>Time<li>Location</ul></p><a href="#"><img alt="Weather"class="img-thumbnail"height="50px"src="assets/images/weather.png"width="50px"></a></div></div></div><div class="col-2"><div class="card"><div class="card-body"><h4 class="card-title">Event</h4><p class="card-text"><ul><li>Description<li>Date<li>Time<li>Location</ul></p><a href="#"><img alt="Weather"class="img-thumbnail"height="50px"src="assets/images/weather.png"width="50px"></a></div></div></div><div class="col-2"><div class="card"><div class="card-body"><h4 class="card-title">Event</h4><p class="card-text"><ul><li>Description<li>Date<li>Time<li>Location</ul></p><a href="#"><img alt="Weather"class="img-thumbnail"height="50px"src="assets/images/weather.png"width="50px"></a></div></div></div><div class="col-2"><div class="card"><div class="card-body"><h4 class="card-title">Event</h4><p class="card-text"><ul><li>Description<li>Date<li>Time<li>Location</ul></p><a href="#"><img alt="Weather"class="img-thumbnail"height="50px"src="assets/images/weather.png"width="50px"></a></div></div></div><div class="col-1"></div>';
+$("#searchGo").on("click", function(){
+
+userSearch = $("#searchBar").val().trim();
+console.log(userSearch);
+
+if (userSearch === "") {
+	return;
+} else {
+	console.log("user search was a string");
+	$("#cardSection").html(cardBuild);
+}
+
+});
+
+});
